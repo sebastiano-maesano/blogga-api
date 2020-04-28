7a216463eecff996859d0452651a86834d375da3
Sebastiano Maesano <s.maesano@beddy.io>
2020-04-28 23:00:16 +0200

    first commit

---
+++ src/main/java/com/blogga/api/bloggaapi/BloggaApiApplication.java
@@ -0,0 +1,13 @@
+package com.blogga.api.bloggaapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BloggaApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BloggaApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/blogga/api/bloggaapi/BloggaApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.blogga.api.bloggaapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BloggaApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
